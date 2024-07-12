e6bfc1eeea0dae76cf7ca3103854e146419aebdc
SampavyKangeyan <sam990731@gmail.com>
2024-07-12 16:07:53 +0530

    Adding AboutUs & ContactUs files

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -1,11 +1,72 @@
-import React from 'react'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 function AboutUs() {
+  
   return (
     <div>
-      
-    </div>
-  )
-}
+      <div className="about-section">
+        <h1>About Us</h1>
+        <h2>Welcome to our Website</h2>
+        <p>Our AgriCure is a multilingual agricultural platform in Sinhala, Tamil, and English for farmers in Sri Lanka, integrating AI technology for accurate disease detection and management</p>
+      <h3>Mission</h3>
+      <p> Empower Sri Lankan farmers with AI-powered crop disease management, bridge the language gap with a multilingual platform in Sinhala, English, and Tamil, and foster a knowledge-sharing community for farmers.</p>
+      <h3>Vision</h3>
+      <p>Revolutionize Sri Lankan agriculture by fostering innovation and sustainability, ensuring food security for future generations, and improving the livelihoods of farmers and the overall well-being of Sri Lankan communities.</p>
+      </div>
 
-export default AboutUs
+      <h2 style={{ textAlign: "center" }}>The Team Behind AgriCure</h2>
+      <div className="row">
+        <div className="column">
+          <div className="card">
+            <img src="C:\Users\DELL\OneDrive\Documents\AgriCure-1\AgriCure-Project\public\images\Hamitha.jpg" alt="K.D.H.Apsara" style={{ width: "100%" }} />
+            <div className="container">
+              <h2>K.D.H.Apsara</h2>
+              <p className="title">UI/UX Designer</p>
+              <p>hamithakd99@gmail.com</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="column">
+          <div className="card">
+            <img src="C:\Users\DELL\OneDrive\Documents\AgriCure-1\AgriCure-Project\public\images\Pawani.jpg" alt="J.A.P.W.Jayaweera" style={{ width: "100%" }} />
+            <div className="container">
+              <h2>J.A.P.W.Jayaweera</h2>
+              <p className="title">Full Stack Developer</p>
+              <p>pawaniw07@gmail.com</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="column">
+          <div className="card">
+            <img src="C:\Users\DELL\OneDrive\Documents\AgriCure-1\AgriCure-Project\public\images\Sampavy.jpg" alt="S.Kangeyan" style={{ width: "100%" }} />
+            <div className="container">
+              <h2>S.Kangeyan</h2>
+              <p className="title">Full Stack Developer</p>
+              <p>sma990731@gmail.com</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="column">
+          <div className="card">
+          <img src="C:\Users\DELL\OneDrive\Documents\AgriCure-1\AgriCure-Project\public\images\Buddini.jpg" alt="Y.S.M.B.U.Yallarawa" style={{ width: "100%" }} />
+            <div className="container">
+              <h2>Y.S.M.B.U.Yallarawa</h2>
+              <p className="title">Full Stack Developer</p>
+              <p>buddhiniyallarawa@gmail.com</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+
+
+  );
+}
+export default AboutUs;
---
+++ src/content_option.js
@@ -0,0 +1,83 @@
+const logotext = 'K.D.H.Apsara';
+const meta = {
+  title: 'K.D.H.Apsara',
+  description:
+    'I’m K.D.H.Apsara UI/UX Designer & Full stack devloper, currently working in Sri Lanka',
+};
+
+const introdata = {
+  title: 'I’m K.D.H.Apsara',
+  animated: {
+    first: 'I love coding',
+    second: 'I code cool websites',
+    third: 'I develop mobile apps',
+  },
+  description:
+    'I’m K.D.H.Apsara UI/UX Designer & Full stack devloper, currently working in Sri Lanka',
+  your_img_url: 'https://images.unsplash.com/photo-1514790193030-c89d266d5a9d',
+};
+
+const dataabout = {
+  title: 'Something about my self',
+  aboutme:
+    'I’m K.D.H.Apsara UI/UX Designer & Full stack devloper, currently working in Sri Lanka',
+};
+const worktimeline = [
+  {
+    jobtitle: 'Designer of week',
+    where: 'YAdfi',
+    date: '2020',
+  },
+  {
+    jobtitle: 'Designer of week',
+    where: 'Jamalya',
+    date: '2019',
+  },
+  {
+    jobtitle: 'Designer of week',
+    where: 'ALquds',
+    date: '2019',
+  },
+];
+
+
+
+const services = [
+  {
+    title: 'UI & UX Design',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.',
+  },
+  {
+    title: 'Mobile Apps',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.',
+  },
+  {
+    title: 'Wordpress Design',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.',
+  },
+];
+
+const contactConfig = {
+YOUR_NAME: 'K.D.H.Apsara',
+  YOUR_EMAIL: 'hamithakd99@gmail.com',
+  YOUR_PHONE: '+94 70 205 3815',
+  description:
+    'UI/UX Designer & Full stack devloper, currently working in Sri Lanka',
+  YOUR_SERVICE_ID: 'service_id',
+  YOUR_TEMPLATE_ID: 'template_id',
+  YOUR_USER_ID: 'user_id',
+};
+
+ 
+export {
+  meta,
+  dataabout,
+  worktimeline,
+  services,
+  introdata,
+  contactConfig, 
+  logotext,
+};
