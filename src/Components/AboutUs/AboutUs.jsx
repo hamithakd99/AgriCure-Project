6d66319bae8986472218522096889a0df3dce100
SampavyKangeyan <sam990731@gmail.com>
2024-07-18 21:48:41 +0530

    Am adding updated AboutUs & ContactUs components

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -1,28 +1,54 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-
+import Hamitha from '../../assets/images/Hamitha.jpg';
+import Pawani from '../../assets/images/Pawani.jpg';
+import Sampavy from '../../assets/images/Sampavy.jpg';
+import Buddini from '../../assets/images/Buddini.jpg';
+import './AboutUs.css'
 
 function AboutUs() {
-  
   return (
-    <div>
+    <div className="container">
       <div className="about-section">
-        <h1>About Us</h1>
-        <h2>Welcome to our Website</h2>
-        <p>Our AgriCure is a multilingual agricultural platform in Sinhala, Tamil, and English for farmers in Sri Lanka, integrating AI technology for accurate disease detection and management</p>
-      <h3>Mission</h3>
-      <p> Empower Sri Lankan farmers with AI-powered crop disease management, bridge the language gap with a multilingual platform in Sinhala, English, and Tamil, and foster a knowledge-sharing community for farmers.</p>
-      <h3>Vision</h3>
-      <p>Revolutionize Sri Lankan agriculture by fostering innovation and sustainability, ensuring food security for future generations, and improving the livelihoods of farmers and the overall well-being of Sri Lankan communities.</p>
-      </div>
-
-      <h2 style={{ textAlign: "center" }}>The Team Behind AgriCure</h2>
-      <div className="row">
+        <h1>About Us</h1><br/>
+        <h4><b>Welcome to our Website</b></h4>
+        <p><br/>
+          Our AgriCure is a multilingual agricultural platform for farmers in Sri Lanka, integrating AI technology for accurate disease detection and management.
+        </p>
+        <div id="about-1">
+          <div class="content-box-lg">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="card-1">
+                  <div class="about-item text-center">
+                    <i class="fa fa-book"></i>
+                      <h3>MISSION</h3>
+                          <p> Empower Sri Lankan farmers with AI-powered crop disease management, bridge the language gap with a multilingual platform in Sinhala, English, and Tamil, and foster a knowledge-sharing community for farmers.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="card-1">
+                  <div class="about-item text-center">
+                    <i class="fa fa-book"></i>
+                      <h3>VISION</h3>
+                      <p> Revolutionize Sri Lankan agriculture by fostering innovation and sustainability, ensuring food security for future generations, and improving the livelihoods of farmers and the overall well-being of Sri Lankan communities.</p>        
+                  </div>
+                </div>
+              </div> 
+            </div>  
+          </div>  
+        </div>
+      <br/>
+      <br/>
+      <h1 style={{ textAlign: "center" }}>The Team Behind AgriCure</h1>
+      <div class="row">
+      <div class="content-box-lg">
         <div className="column">
           <div className="card">
-            <img src="C:\Users\DELL\OneDrive\Documents\AgriCure-1\AgriCure-Project\public\images\Hamitha.jpg" alt="K.D.H.Apsara" style={{ width: "100%" }} />
-            <div className="container">
-              <h2>K.D.H.Apsara</h2>
+            <img src={Hamitha} alt="K.D.H.Apsara" style={{ width: "70%" }} />
+            <div className="container-1">
+              <h4>K.D.H.Apsara</h4>
               <p className="title">UI/UX Designer</p>
               <p>hamithakd99@gmail.com</p>
             </div>
@@ -31,42 +57,39 @@ function AboutUs() {
 
         <div className="column">
           <div className="card">
-            <img src="C:\Users\DELL\OneDrive\Documents\AgriCure-1\AgriCure-Project\public\images\Pawani.jpg" alt="J.A.P.W.Jayaweera" style={{ width: "100%" }} />
-            <div className="container">
-              <h2>J.A.P.W.Jayaweera</h2>
+            <img src={Pawani} alt="J.A.P.W.Jayaweera" style={{ width: "70%" }} />
+            <div className="container-1">
+              <h4>J.A.P.W.Jayaweera</h4>
               <p className="title">Full Stack Developer</p>
               <p>pawaniw07@gmail.com</p>
             </div>
           </div>
         </div>
-
         <div className="column">
           <div className="card">
-            <img src="C:\Users\DELL\OneDrive\Documents\AgriCure-1\AgriCure-Project\public\images\Sampavy.jpg" alt="S.Kangeyan" style={{ width: "100%" }} />
-            <div className="container">
-              <h2>S.Kangeyan</h2>
+            <img src={Sampavy} alt="S.Kangeyan" style={{ width: "70%" }} />
+            <div className="container-1">
+              <h4>S.Kangeyan</h4>
               <p className="title">Full Stack Developer</p>
-              <p>sma990731@gmail.com</p>
+              <p>sam990731@gmail.com</p><br/>
             </div>
           </div>
         </div>
-
         <div className="column">
           <div className="card">
-          <img src="C:\Users\DELL\OneDrive\Documents\AgriCure-1\AgriCure-Project\public\images\Buddini.jpg" alt="Y.S.M.B.U.Yallarawa" style={{ width: "100%" }} />
-            <div className="container">
-              <h2>Y.S.M.B.U.Yallarawa</h2>
+            <img src={Buddini} alt="Y.S.M.B.U.Yallarawa" style={{ width: "70%"}} />
+            <div className="container-1">
+              <h4>Y.S.M.B.U.Yallarawa</h4>
               <p className="title">Full Stack Developer</p>
               <p>buddhiniyallarawa@gmail.com</p>
             </div>
           </div>
         </div>
       </div>
-
-
+      </div>
+      </div>
     </div>
-
-
   );
 }
+
 export default AboutUs;
--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -1,94 +1,180 @@
-import React from 'react'
-import { Container, Row, Col } from "react-bootstrap";
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import emailjs from '@emailjs/browser';
 import 'bootstrap/dist/css/bootstrap.css';
-import { contactConfig } from '../../content_option';
+import './ContactUs.css'
 
-export default function ContactUs() {
+
+const ContactUs = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors }
+  } = useForm();
+  const [disabled, setDisabled] = useState(false);
+  const [alertInfo, setAlertInfo] = useState({
+    display: false,
+    message: '',
+    type: '',
+  });
   
+  
+  // Shows alert message for form submission feedback
+  const toggleAlert = (message, type) => {
+    setAlertInfo({ display: true, message, type });
 
-  return (
-      <Container>
-     
-        <Row className="mb-5 mt-3">
-          <Col lg="8">
-            <h1 className="display-4 mb-4">Contact Us</h1>
-            <hr className="t_border my-4 ml-0 text-left" />
-          </Col>
-        </Row>
-        <Row className="sec_sp">
-          <Col lg="5" className="mb-5">
-            <h3 className="color_sec py-4">Get in touch</h3>
-            <name>
-            {contactConfig.hasOwnProperty("YOUR_NAME") ? (
-                <p>
-                  <strong>Name:</strong> {contactConfig.YOUR_NAME}
-                </p>
-              ) : (
-                ""
-              )}
-            </name>
-            
-            <address>
-              <strong>Email:</strong>{" "}
-              <a href={`mailto:${contactConfig.YOUR_EMAIL}`}>
-                {contactConfig.YOUR_EMAIL}
-              </a>
-              <br />
-              <br />
-              {contactConfig.hasOwnProperty("YOUR_PHONE") ? (
-                <p>
-                  <strong>Phone:</strong> {contactConfig.YOUR_PHONE}
-                </p>
-              ) : (
-                ""
-              )}
-            </address>
-            <p>{contactConfig.description}</p>
-          </Col>
-          <Col lg="7" className="d-flex align-items-center">
-            <form  className="contact__form w-100">
-              <Row>
-                <Col lg="6" className="form-group">
-                  <input
-                    className="form-control"
-                    id="name"
-                    name="name"
-                    placeholder="Name" 
-                    type="text"
-                    required 
-                  />
-                </Col>
-                <Col lg="6" className="form-group">
-                  <input
-                    className="form-control rounded-0"
-                    id="email"
-                    name="email"
-                    placeholder="Email"
-                    type="email" 
-                    required 
-                  />
-                </Col>
-              </Row>
-              <textarea
-                className="form-control rounded-0"
-                id="message"
-                name="message"
-                placeholder="Message"
-                rows="5" 
-                required
-              ></textarea>
-              <br />
-              <Row>
-                <Col lg="12" className="form-group">
-                <button class="btn btn-primary">
-                  Send
-                  </button>
-                </Col>
-              </Row>
-            </form>
-          </Col>
-        </Row>
-      </Container>
+    // Hide alert after 5 seconds
+    setTimeout(() => {
+      setAlertInfo({ display: false, message: '', type: '' });
+    }, 5000);
+  };
+  
+  // Function called on submit that uses emailjs to send email of valid contact form
+  const onSubmit = async (data) => {
+    // Destrcture data object
+    const { name, email, subject, message } = data;
+    try {
+      // Disable form while processing submission
+      setDisabled(true);
+      
+      const templateParams = {
+        name,
+        email,
+        subject,
+        message
+      };
+
+      emailjs.send('service_AgriCure-4', 'template_ur831ln', templateParams, '7BLfeUFDIqTK5sjOi').then(
+        (response) => {
+          console.log('SUCCESS!', response.status, response.text);
+        },
+        (error) => {
+          console.log('FAILED...', error);
+        },
+      );
+
+     // Display success alert
+    toggleAlert('Form submission was successful!', 'success');
+    } catch (e) {
+      console.error(e);
+      // Display error alert
+      toggleAlert('Uh oh. Something went wrong.', 'danger');
+    } finally {
+      // Re-enable form submission
+      setDisabled(false);
+      // Reset contact form fields after submission
+      reset();
+    }
+  };
+ 
+return (
+    <div className='ContactForm'>
+      <div className='container'>
+        <h2>Contact us</h2>
+        <div className='row'>
+          <div className='col-12 text-center'>
+            <div className='contactForm'>
+              <form id='contact-form' onSubmit={handleSubmit(onSubmit)} noValidate>
+                {/* Row 1 of form */}
+                <div className='row formRow'>
+                  <div className='col-6'>
+                    <input
+                      type='text'
+                      name='name'
+                      {...register('name', {
+                        required: { value: true, message: 'Please enter your name' },
+                        maxLength: {
+                          value: 30,
+                          message: 'Please use 30 characters or less'
+                        }
+                      })}
+                      className='form-control formInput'
+                      placeholder='Name'
+                    ></input>
+                    {errors.name && <span className='errorMessage'>{errors.name.message}</span>}
+                  </div>
+                  <div className='col-6'>
+                    <input
+                      type='email'
+                      name='email'
+                      {...register('email', {
+                        required: true,
+                        pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+                      })}
+                      className='form-control formInput'
+                      placeholder='Email address'
+                    ></input>
+                    {errors.email && (
+                      <span className='errorMessage'>Please enter a valid email address</span>
+                    )}
+                  </div>
+                </div>
+                {/* Row 2 of form */}
+                <div className='row formRow'>
+                  <div className='col'>
+                    <input
+                      type='text'
+                      name='subject'
+                      {...register('subject', {
+                        required: { value: true, message: 'Please enter a subject' },
+                        maxLength: {
+                          value: 75,
+                          message: 'Subject cannot exceed 75 characters'
+                        }
+                      })}
+                      className='form-control formInput'
+                      placeholder='Subject'
+                    ></input>
+                    {errors.subject && (
+                      <span className='errorMessage'>{errors.subject.message}</span>
+                    )}
+                  </div>
+                </div>
+                {/* Row 3 of form */}
+                <div className='row formRow'>
+                  <div className='col'>
+                    <textarea
+                      rows={3}
+                      name='message'
+                      {...register('message', {
+                        required: true
+                      })}
+                      className='form-control formInput'
+                      placeholder='Message'
+                    ></textarea>
+                    {errors.message && <span className='errorMessage'>Please enter a message</span>}
+                  </div>
+                </div>
+                <button className='submit-btn' type='submit'>
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+        {alertInfo.display && (
+          <div
+            className={`alert alert-${alertInfo.type} alert-dismissible mt-5`}
+            role='alert'
+          >
+            {alertInfo.message}
+            <button
+              type='button'
+              className='btn-close'
+              data-bs-dismiss='alert'
+              aria-label='Close'
+              onClick={() =>
+                setAlertInfo({ display: false, message: '', type: '' })
+              } // Clear the alert when close button is clicked
+            ></button>
+          </div>
+        )}
+    </div>
+    
   );
-}
+  
+};
 
+export default ContactUs;
